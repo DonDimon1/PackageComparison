99faddd6c97e2ecf08523b72100c81c6bc92d674
DonDimon1 <dmpav1@yandex.ru>
2024-07-06 01:50:27 +0400

    instead of boost, try curl

--- main.cpp
+++ main.cpp
@@ -1,14 +1,62 @@
 #include <iostream>
-#include <boost/version.hpp>
+#include <curl/curl.h>
 
-using namespace std;
+// Функция обратного вызова для записи данных
+size_t write_callback(void *ptr, size_t size, size_t nmemb, std::string *data) {
+    data->append((char *)ptr, size * nmemb);
+    return size * nmemb;
+}
 
-int main()
-{
-    std::cout << "Boost version: "
-                  << BOOST_VERSION / 100000 << "."  // major version
-                  << BOOST_VERSION / 100 % 1000 << "."  // minor version
-                  << BOOST_VERSION % 100  // patch level
-                  << std::endl;
+int main() {
+    CURL *curl;
+    CURLcode res;
+    std::string url = "https://rdb.altlinux.org/api/version";
+    std::string response;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        // Добавление SSL версии
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        } else {
+            long response_code;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+            std::cout << "Response code: " << response_code << std::endl;
+            std::cout << "Response body:\n" << response << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
